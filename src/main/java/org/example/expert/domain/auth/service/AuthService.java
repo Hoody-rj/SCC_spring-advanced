8548c217e36f486d564c4e56a2e12f22465ec825
Shaun.the.sheep <robin.Jeong817@gmail.com>
2025-07-21 20:18:51 +0900

    2025-07-21
    fix : 존재하는 이메일을 확인하는 IF 구문의 위치를 signup함수의 헤드 부분으로 이동하여 password를 인코딩을 필요한 경우에만 진행하도록 변경

--- src/main/java/org/example/expert/domain/auth/service/AuthService.java
+++ src/main/java/org/example/expert/domain/auth/service/AuthService.java
@@ -25,15 +25,24 @@ public class AuthService {
 
     @Transactional
     public SignupResponse signup(SignupRequest signupRequest) {
-
-        String encodedPassword = passwordEncoder.encode(signupRequest.getPassword());
-
-        UserRole userRole = UserRole.of(signupRequest.getUserRole());
-
+        //2025-07-21
+        //여기로 위치 변경
+        //이메일 확인하고 없을 경우 모든 작업 불필요
         if (userRepository.existsByEmail(signupRequest.getEmail())) {
             throw new InvalidRequestException("이미 존재하는 이메일입니다.");
         }
 
+        String encodedPassword = passwordEncoder.encode(signupRequest.getPassword());
+
+        UserRole userRole = UserRole.of(signupRequest.getUserRole());
+        //region Early Return에 대한 리팩토링 문제 구간
+        //
+        //해당 에러가 발생하는 상황일 때, passwordEncoder의 encode() 동작이 불필요하게 일어나지 않게 코드를 개선해주세요.
+
+//        if (userRepository.existsByEmail(signupRequest.getEmail())) {
+//            throw new InvalidRequestException("이미 존재하는 이메일입니다.");
+//        }
+        //endregion
         User newUser = new User(
                 signupRequest.getEmail(),
                 encodedPassword,
