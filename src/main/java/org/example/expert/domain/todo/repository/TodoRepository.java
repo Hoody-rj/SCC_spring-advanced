09a34c75681e3ff11dc81d8be94625ac121f27ad
Shaun.the.sheep <robin.Jeong817@gmail.com>
2025-07-24 10:06:58 +0900

    2025-07-23
    fix : EntityGraph 사용으로 Fetch Join 쿼리삭제

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -1,8 +1,10 @@
 package org.example.expert.domain.todo.repository;
 
+import jakarta.persistence.Entity;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,12 +13,21 @@ import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
+
+    //Lv2.Fetch Join 문제
+
+    //Code Before
+    //@Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
+    @EntityGraph(attributePaths = {"managers"})
+    @Query("SELECT t FROM Todo t ORDER BY t.modifiedAt")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN FETCH t.user " +
-            "WHERE t.id = :todoId")
+    //Code Before
+//    @Query("SELECT t FROM Todo t " +
+//            "LEFT JOIN FETCH t.user " +
+//            "WHERE t.id = :todoId")
+    @EntityGraph(attributePaths = {"managers"})
+    @Query("SELECT t FROM Todo t where t.user = :todoId")
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 
     int countById(Long todoId);
